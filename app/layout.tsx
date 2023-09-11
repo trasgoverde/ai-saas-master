67cf79478fff3b31db4f9d93d3926ed5065a8fa7
Ignacio <ign.souto@gmail.com>
2023-09-11 17:38:18 +0200

    UI changes :draw:

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import './globals.css'
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Dipassio',
+  title: 'Dipassio | AI Platform',
   description: 'AI Platform',
 }
 
