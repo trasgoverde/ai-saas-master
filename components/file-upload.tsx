e5d76bcef4ebbf27552ff7e699660b8e5d1c6bfb
Ignacio <ign.souto@gmail.com>
2023-09-26 13:21:34 +0200

    Upload to AWS Bucket correct :100:

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -3,9 +3,29 @@
 import React from "react";
 import { useDropzone } from "react-dropzone";
 import { Inbox, Loader2 } from "lucide-react";
+import { uploadToS3 } from "@/lib/s3";
 
 const FileUpload = () => {
-    const { getRootProps, getInputProps} = useDropzone();
+    const { getRootProps, getInputProps} = useDropzone({
+        accept: { "application/pdf": [".pdf"]},
+        maxFiles: 1,
+        onDrop: async (acceptedFiles) => {
+            console.log(acceptedFiles);
+            const file = acceptedFiles[0]
+            if (file.size > 10 * 1024 * 1024){
+                //Bigger than 10mb!!
+                alert('Please, Upload a Smaller File')
+                return
+            }
+
+            try {
+                const data = await uploadToS3(file);
+                console.log("data", data);
+                } catch (error) {
+                    console.log(error);
+            }
+        },
+    });
     return (
         <div className="p-2 bg-white rounded-x1">
             <div 
