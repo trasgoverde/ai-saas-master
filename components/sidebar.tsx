12187ea8cfde3454827cc1d8c097d92eb4ffb2c9
Ignacio <ign.souto@gmail.com>
2023-09-28 17:17:06 +0200

    brain

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -75,7 +75,7 @@ export const Sidebar = ({
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative h-8 w-8 mr-4">
-            <Image fill alt="Logo" src="/rhinologo best.svg" />
+            <Image fill alt="Logo" src="/brain-right.png" />
           </div>
           <h1 className={cn("text-2xl font-bold", poppins.className)}>
             DIPASSIO AI
