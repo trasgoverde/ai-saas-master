a88ec953a98fed7106c35cd0e81eb0b88f9d2c7c
Ignacio <ign.souto@gmail.com>
2023-10-03 12:06:03 +0200

    Changes Packets and handle errors  API Chat :fire:

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
     const { messages, chatId } = await req.json();
     const _chats = await db.select().from(chats).where(eq(chats.id, chatId));
     if (_chats.length != 1) {
-      return NextResponse.json({ error: "chat not found" }, { status: 404 });
+      return NextResponse.json({ error: "Chat not found" }, { status: 404 });
     }
     const fileKey = _chats[0].fileKey;
     const lastMessage = messages[messages.length - 1];
@@ -37,7 +37,7 @@ export async function POST(req: Request) {
       END OF CONTEXT BLOCK
       AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
       If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-      AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+      AI assistant will not apologize for previous responses, but instead will indicate new information was gained.
       AI assistant will not invent anything that is not drawn directly from the context.
       `,
     };
@@ -50,9 +50,10 @@ export async function POST(req: Request) {
       ],
       stream: true,
     });
+
     const stream = OpenAIStream(response, {
       onStart: async () => {
-        // save user message into db
+        // Save user message into the database
         await db.insert(_messages).values({
           chatId,
           content: lastMessage.content,
@@ -60,7 +61,7 @@ export async function POST(req: Request) {
         });
       },
       onCompletion: async (completion) => {
-        // save ai message into db
+        // Save AI message into the database
         await db.insert(_messages).values({
           chatId,
           content: completion,
@@ -68,6 +69,11 @@ export async function POST(req: Request) {
         });
       },
     });
+
     return new StreamingTextResponse(stream);
-  } catch (error) {}
+  } catch (error) {
+    // Handle the error and return an appropriate response
+    console.error("Error in POST:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
